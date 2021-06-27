75407265135cd9a3ecaa42605c40b165a71730ca
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2021-06-27 15:13:45 +0530

    cerated shows and actor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,58 +3,89 @@ import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
-    const [input, setInput] = useState('');
-    const [results, setResults] = useState(null);
+        const [input, setInput] = useState('');
+        const [results, setResults] = useState(null);
+        const [searchOption, setSearchOption] = useState('shows');
 
-    const onSearch = () => {
-        apiGet(`/search/shows?q=${input}`).then(result => {
-            setResults(result);
-        });
-    };
+        const isShowsSearch = searchOption === 'shows';
 
-    const onInputChange = ev => {
-        setInput(ev.target.value);
-    };
+        const onSearch = () => {
+            apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+                setResults(result);
+            });
+        };
 
-    const onKeyDown = ev => {
-        if (ev.keyCode === 13) {
-            onSearch();
-        }
-    };
+        const onInputChange = ev => {
+            setInput(ev.target.value);
+        };
 
-    const renderResults = () => {
-        if (results && results.length === 0) {
-            return <div > No results < /div>;
-        }
+        const onKeyDown = ev => {
+            if (ev.keyCode === 13) {
+                onSearch();
+            }
+        };
 
-        if (results && results.length > 0) {
+        const onRadioChange = ev => {
+            setSearchOption(ev.target.value);
+        };
+
+        const renderResults = () => {
+            if (results && results.length === 0) {
+                return <div > No results < /div>;
+            }
+
+            if (results && results.length > 0) {
+                return results[0].show ?
+                    results.map(item => < div key = { item.show.id } > { item.show.name } < /div>): results.map(item => ( <
+                            div key = { item.person.id } > { item.person.name } < /div>
+                        ));
+                    }
+
+                return null;
+            };
+
             return ( <
-                div > {
-                    results.map(item => ( <
-                        div key = { item.show.id } > { item.show.name } < /div>
-                    ))
-                } <
+                MainPageLayout >
+                <
+                input type = "text"
+                placeholder = "Search for something"
+                onChange = { onInputChange }
+                onKeyDown = { onKeyDown }
+                value = { input }
+                />
+
+                <
+                div >
+                <
+                label htmlFor = "shows-search" >
+                Shows <
+                input id = "shows-search"
+                type = "radio"
+                value = "shows"
+                checked = { isShowsSearch }
+                onChange = { onRadioChange }
+                /> <
+                /label>
+
+                <
+                label htmlFor = "actors-search" >
+                Actors <
+                input id = "actors-search"
+                type = "radio"
+                value = "people"
+                checked = {!isShowsSearch }
+                onChange = { onRadioChange }
+                /> <
+                /label> <
                 /div>
+
+                <
+                button type = "button"
+                onClick = { onSearch } >
+                Search <
+                /button> { renderResults() } <
+                /MainPageLayout>
             );
-        }
+        };
 
-        return null;
-    };
-
-    return ( <
-        MainPageLayout >
-        <
-        input type = "text"
-        onChange = { onInputChange }
-        onKeyDown = { onKeyDown }
-        value = { input }
-        /> <
-        button type = "button"
-        onClick = { onSearch } >
-        Search <
-        /button> { renderResults() } < /
-        MainPageLayout >
-    );
-};
-
-export default Home;
+        export default Home;
