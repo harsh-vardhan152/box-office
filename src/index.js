72fb633806cce8bbbc1d597978daaa49c0afe80c
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2021-06-27 21:28:40 +0530

    final

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
+ReactDOM.render( <
+    HashRouter >
+    <
+    App / >
+    <
+    /HashRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
